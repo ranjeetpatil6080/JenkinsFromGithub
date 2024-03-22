6e1b0777e327d1a540131335cd3fd65014f09b7a
ranje <ranje@Ranjeet>
2024-03-22 22:07:59 +0530

    first try for github to jenkins

---
+++ src/test/java/AdvancePackage/test1.java
@@ -0,0 +1,21 @@
+package AdvancePackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class test1 {
+	
+	@Test
+	public void CreateOrgTest()
+	{
+		WebDriver driver=new ChromeDriver();
+		
+		driver.get("http://localhost:8888/");
+		driver.findElement(By.name("user_name")).sendKeys("admin");
+		driver.findElement(By.name("user_password")).sendKeys("admin");
+		driver.findElement(By.id("submitButton")).click();
+	}
+
+}
