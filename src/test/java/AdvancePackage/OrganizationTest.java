7d9fc28e274d2bae42825fa642d9688aa8ad9c48
ranjeetpatil6080 <154863613+ranjeetpatil6080@users.noreply.github.com>
2024-03-23 09:38:19 +0530

    Update and rename test1.java to OrganizationTest.java

--- src/test/java/AdvancePackage/OrganizationTest.java
+++ src/test/java/AdvancePackage/OrganizationTest.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class test1 {
+public class OrganizationTest {
 	
 	@Test
 	public void CreateOrgTest()
